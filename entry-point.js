e500deef877a3505d5cf250e93bbfb007803e118
K Makesh Kumar Naidu <59633482+makeshnaidu@users.noreply.github.com>
2020-04-02 07:19:46 -0700

    Adding entry point view

--- entry-point.js
+++
@@ -1,3 +0,0 @@
-const entry_point = () => (req, res) => res.send("Hello from Entry_Point Module!");
-
-module.exports = entry_point;
---
+++ routes/entry-point.js
@@ -0,0 +1,6 @@
+const entry_point = (req, res) => {
+    res.render('entry-point')
+}
+    // res.send("Hello from Entry_Point Module!");
+
+module.exports = entry_point;
