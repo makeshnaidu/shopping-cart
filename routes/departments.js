2480799793c13a9059eab4f88d62560b8a3712a4
K Makesh Kumar Naidu <59633482+makeshnaidu@users.noreply.github.com>
2020-04-02 19:29:05 -0700

    Adding Product array to department route

--- routes/departments.js
+++ routes/departments.js
@@ -1,5 +1,42 @@
 const departments = (req, res) => {
-    res.render('departments')
+
+    const products = [
+        {
+            id:1,
+            name: "Fender",
+            description: "Falcon 5 elite gaming headset with LED lights",
+            price: 199.99,
+            rating: 5,
+            image: "https://assets.shop.loblaws.ca/products_jpeg/20904659/en/20904659_sml_1_@1x.jpg"
+        },
+        {
+            id:1,
+            name: "HeadPhone",
+            description: "Polaroid Premium Wired Headphoness",
+            price: 19.99,
+            rating: 5,
+            image: "https://assets.shop.loblaws.ca/products_jpeg/20904659/en/20904659_sml_1_@1x.jpg"
+        },
+        {
+            id:1,
+            name: "Sony",
+            description: "SonyHeadphones, Black MDRZX110B1 ea",
+            price: 29.99,
+            rating: 5,
+            image: "https://assets.sellers.loblaw.ca/products/all/1090/29909_1.jpg?size=274"
+        },
+        {
+            id:1,
+            name: "Audio Cable",
+            description: "AudiovoxHDMI Cable, 12 ft",
+            price: 9.99,
+            rating: 5,
+            image: "https://assets.sellers.loblaw.ca/products/all/1090/29863_1.jpg?size=274"
+        }
+        
+    ]
+
+    res.render('departments', {products})
 }
 
 
