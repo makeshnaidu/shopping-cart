4492bfdb87b46a8da69d3befe035a7f4984e256e
makesh <makesh.naidu@gmail.com>
2020-04-06 11:49:45 -0700

    configuring uri

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ if(port == null || port =="") {
 }
 
 //Database Connection
-const uri = process.env.MONGODB_URL;
+//const uri = process.env.MONGODB_URL;
+const uri = "mongodb://shoppingcart:shoppingcart1234@cluster0-shard-00-00-v7o5p.mongodb.net:27017,cluster0-shard-00-01-v7o5p.mongodb.net:27017,cluster0-shard-00-02-v7o5p.mongodb.net:27017/shoppingcart?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
     app.listen(port, () => console.log(`Shopping cart running on port ${port}!`))
     
