c31c089a503956fb359ff9bf98fc8035bc4c91e3
makesh <makesh.naidu@gmail.com>
2020-04-06 11:20:04 -0700

    Refactoring dotenv congig

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-require('dotenv/config');
+//require('dotenv/config');
+require('dotenv').config({ path: './.env' });
 const express = require('express')
 var bodyParser = require('body-parser')
 const mongoose = require('mongoose')
