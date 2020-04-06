cf6379f87ee996ea0079b677974934630957871c
K Makesh Kumar Naidu <59633482+makeshnaidu@users.noreply.github.com>
2020-04-05 17:25:31 -0700

    Refactoring delete.js

--- routes/admin/delete.js
+++ routes/admin/delete.js
@@ -1,17 +1,12 @@
-const mongoose = require('mongoose');
 const Product = require('../../models/product')
-const uri = "mongodb://shoppingcart:shoppingcart1234@cluster0-shard-00-00-v7o5p.mongodb.net:27017,cluster0-shard-00-01-v7o5p.mongodb.net:27017,cluster0-shard-00-02-v7o5p.mongodb.net:27017/shoppingcart?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 
 const adminDelete = async (req, res) => {
     const idToDelete = req.params.id;
-    const document = await Product.findById(idToDelete).exec();
+    await Product.deleteOne({ _id: idToDelete }).exec();
+    
     // //Delete the image
     // deleteImage(document.image);
-    //Delete object from database
-    await Product.deleteOne({ _id: idToDelete }).exec();
     res.redirect('/admin')
 }
-
 
 module.exports = adminDelete;
